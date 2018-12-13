a3c6bcb67a7073c772da896b795a7642eb0dc2e4
kaihuu <uemura-toshiaki-xh@ynu.jp>
2018-12-13 16:14:23 +0900

    first commit

--- QueryTestCHORALE/Program.cs
+++ QueryTestCHORALE/Program.cs
@@ -1,15 +1,83 @@
-﻿using System;
+﻿using SensorLogInserterRe.Daos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace QueryTestCHORALE
 {
     class Program
     {
+
+        volatile static bool _keyReaded = false;
+        DataTable dataTable = new DataTable();
+
         static void Main(string[] args)
         {
+            ExecuteMethod();
+            Console.ReadKey(true);
+        }
+
+        static void ExecuteMethod()
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(MyHandler), null);
+
+            for (; ; )
+            {
+                if (_keyReaded)
+                {
+                    Console.WriteLine("キーが押されました");
+                    break;
+                }
+
+                Console.Write("Sleep in....");
+                for (int i = 0; i < 1; i++)
+                {
+                    Task.Run(() => ExecuteCHORALEQuery(1, 1, "homeward"));
+                }
+                
+                Console.WriteLine("end.");
+                break;
+            }
+            
+        }
+
+        static void MyHandler(object userState)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            _keyReaded = true;
+        }
+
+        static void Anything()
+        {
+
+            Console.WriteLine("Hello");
+            Thread.Sleep(1000);
+
+        }
+
+        static void ExecuteCHORALEQuery(int semanticLinkID, int driverID, string tripDirection)
+        {
+            string query = "SELECT COUNT(*), SUM(LOST_ENERGY)"+
+            "FROM ECOLOG_Doppler_NotMM, SEMANTIC_LINKS" +
+            "WHERE ECOLOG_Doppler_NotMM.DRIVER_ID = " + driverID + "AND SEMANTIC_LINKS.SEMANTIC_LINK_ID = " +
+            semanticLinkID + "AND SEMANTIC_LINKS.LINK_ID = ECOLOG_Doppler_NotMM.LINK_ID" +
+            "AND ECOLOG_Doppler_NotMM.TRIP_DIRECTION = "+ tripDirection + 
+            "GROUP BY TRIP_ID";
+
+            //Stopwatchオブジェクトを作成する
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            //ストップウォッチを開始する
+            sw.Start();
+
+            //クエリ実行
+            DatabaseAccesser.GetResult(query);
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
         }
     }
 }
---
+++ QueryTestCHORALE/Utils/DataTableUtil.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensorLogInserterRe.Daos;
+
+namespace SensorLogInserterRe.Utils
+{
+    static class DataTableUtil
+    {
+        public static DataTable GetTripsRawTable()
+        {
+            DataTable tripsRawTable = new DataTable();
+
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            tripsRawTable.Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+            tripsRawTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+            tripsRawTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+            tripsRawTable.Columns.Add(new DataColumn("START_TIME", typeof(DateTime)));
+            tripsRawTable.Columns.Add(new DataColumn("END_TIME", typeof(DateTime)));
+            tripsRawTable.Columns.Add(new DataColumn("START_LATITUDE", typeof(double)));
+            tripsRawTable.Columns.Add(new DataColumn("START_LONGITUDE", typeof(double)));
+            tripsRawTable.Columns.Add(new DataColumn("END_LATITUDE", typeof(double)));
+            tripsRawTable.Columns.Add(new DataColumn("END_LONGITUDE", typeof(double)));
+
+            return tripsRawTable;
+        }
+
+        public static DataTable GetTripsTable()
+        {
+            DataTable tripsTable = new DataTable();
+
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            tripsTable.Columns.Add(new DataColumn("TRIP_ID", typeof(int)));
+            tripsTable.Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+            tripsTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+            tripsTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+            tripsTable.Columns.Add(new DataColumn("START_TIME", typeof(DateTime)));
+            tripsTable.Columns.Add(new DataColumn("END_TIME", typeof(DateTime)));
+            tripsTable.Columns.Add(new DataColumn("START_LATITUDE", typeof(double)));
+            tripsTable.Columns.Add(new DataColumn("START_LONGITUDE", typeof(double)));
+            tripsTable.Columns.Add(new DataColumn("END_LATITUDE", typeof(double)));
+            tripsTable.Columns.Add(new DataColumn("END_LONGITUDE", typeof(double)));
+            tripsTable.Columns.Add(new DataColumn("CONSUMED_ENERGY", typeof(Single)));
+            tripsTable.Columns.Add(new DataColumn("TRIP_DIRECTION", typeof(string)));
+            tripsTable.Columns.Add(new DataColumn("VALIDATION", typeof(string)));
+
+            return tripsTable;
+        }
+
+        public static DataTable GetAndroidGpsRawDopplerTable()
+        {
+            // TODO 並び順をデータベース通りに
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            DataTable gpsRawTable = new DataTable();
+
+            gpsRawTable.Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("JST", typeof(DateTime)));
+            gpsRawTable.Columns.Add(new DataColumn("LATITUDE", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("LONGITUDE", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("ALTITUDE", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("ACCURACY", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("SPEED", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("BEARING", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("ANDROID_TIME", typeof(DateTime)));
+
+            return gpsRawTable;
+        }
+
+        public static DataTable GetAndroidGpsRawTable()
+        {
+            // TODO 並び順をデータベース通りに
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            DataTable gpsRawTable = new DataTable();
+
+            gpsRawTable.Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+            gpsRawTable.Columns.Add(new DataColumn("JST", typeof(DateTime)));
+            gpsRawTable.Columns.Add(new DataColumn("LATITUDE", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("LONGITUDE", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("ALTITUDE", typeof(double)));
+            gpsRawTable.Columns.Add(new DataColumn("ANDROID_TIME", typeof(DateTime)));
+
+            return gpsRawTable;
+        }
+
+        public static DataTable[] GetAndroidGpsRawTableArray(int n)
+        {
+            // TODO 並び順をデータベース通りに
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            DataTable[] gpsRawTable = new DataTable[n];
+            for (int i = 0; i < n; i++)
+            {
+                gpsRawTable[i] = new DataTable();
+                gpsRawTable[i].Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("JST", typeof(DateTime)));
+                gpsRawTable[i].Columns.Add(new DataColumn("LATITUDE", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("LONGITUDE", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("ALTITUDE", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("ANDROID_TIME", typeof(DateTime)));
+            }
+            return gpsRawTable;
+        }
+
+        public static DataTable[] GetAndroidGpsRawDopplerTableArray(int n)
+        {
+            // TODO 並び順をデータベース通りに
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            DataTable[] gpsRawTable = new DataTable[n];
+            for (int i = 0; i < n; i++)
+            {
+                gpsRawTable[i] = new DataTable();
+                gpsRawTable[i].Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("JST", typeof(DateTime)));
+                gpsRawTable[i].Columns.Add(new DataColumn("LATITUDE", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("LONGITUDE", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("ALTITUDE", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("ACCURACY", typeof(int)));
+                gpsRawTable[i].Columns.Add(new DataColumn("SPEED", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("BEARING", typeof(double)));
+                gpsRawTable[i].Columns.Add(new DataColumn("ANDROID_TIME", typeof(DateTime)));
+            }
+            return gpsRawTable;
+        }
+
+        public static DataTable GetAndroidAccRawTable()
+        {
+            // TODO 並び順をデータベース通りに
+            // TODO string 直書きでなく、Daoのstaticフィールドを参照に
+            DataTable accRawTable = new DataTable();
+
+            accRawTable.Columns.Add(new DataColumn("DRIVER_ID", typeof(int)));
+            accRawTable.Columns.Add(new DataColumn("CAR_ID", typeof(int)));
+            accRawTable.Columns.Add(new DataColumn("SENSOR_ID", typeof(int)));
+            accRawTable.Columns.Add(new DataColumn("DATETIME", typeof(DateTime)));
+            accRawTable.Columns.Add(new DataColumn("ACC_X", typeof(Single)));
+            accRawTable.Columns.Add(new DataColumn("ACC_Y", typeof(Single)));
+            accRawTable.Columns.Add(new DataColumn("ACC_Z", typeof(Single)));
+
+            return accRawTable;
+        }
+
+
+
+
+
+        public static DataTable GetTempCorrectedAccTable(DataTable table)
+        {
+            table.Columns.Add(new DataColumn("ROLL", typeof(Single)));
+            table.Columns.Add(new DataColumn("PITCH", typeof(Single)));
+            table.Columns.Add(new DataColumn("YAW", typeof(Single)));
+
+            //補正データを記録するためのカラムを追加
+            table.Columns.Add(new DataColumn("ALPHA", typeof(Single)));
+            table.Columns.Add(new DataColumn("VECTOR_X", typeof(Single)));
+            table.Columns.Add(new DataColumn("VECTOR_Y", typeof(Single)));
+            table.Columns.Add(new DataColumn("BETA", typeof(Single)));
+            table.Columns.Add(new DataColumn("GAMMA", typeof(Single)));
+
+            return table;
+        }
+
+
+    }
+}
